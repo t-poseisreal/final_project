5648a04787fc566b5f0608cb8d4a2dbc24885c36
teo <bukovcanteo@gmail.com>
2025-05-20 19:18:38 +0200

    additions of quiz cards

---
+++ script.js
@@ -0,0 +1,27 @@
+function rotate_card(){
+    const card = document.getElementById('card_inner')
+    if(card.style.transform  ===''){
+        card.style.transform = 'rotateY(180deg)'
+    } else {
+        card.style.transform = ''
+    }
+
+}
+function rotate_card2(){
+    const card = document.getElementById('card_inner2')
+    if(card.style.transform  ===''){
+        card.style.transform = 'rotateY(180deg)'
+    } else {
+        card.style.transform = ''
+    }
+
+}
+function rotate_card3(){
+    const card = document.getElementById('card_inner3')
+    if(card.style.transform  ===''){
+        card.style.transform = 'rotateY(180deg)'
+    } else {
+        card.style.transform = ''
+    }
+
+}
